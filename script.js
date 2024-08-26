948bec075236a4ac6c88e78092eb82b30b7b5558
Harry Stott <harrystott.dev@gmail.com>
2024-08-26 18:04:40 +0100

    add hover sound effects

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 
 window.onload = function() {
     function update() {
-        var date = new Date();
-        var londonTime = date.toLocaleTimeString("en-GB", {timeZone: "Europe/London"});
-        document.getElementById("time").innerHTML = londonTime;
+        updateTime()
     }
         update()
         setInterval(update, 1000);
 };
+
+function updateTime(){
+    var date = new Date();
+    var londonTime = date.toLocaleTimeString("en-GB", {timeZone: "Europe/London"});
+    document.getElementById("time").innerHTML = londonTime;
+}
+
+function buttonHover(){
+    let clickSound = new Audio('sounds/interface-button-154180.mp3');
+    clickSound.play();
+}
 
 
 
