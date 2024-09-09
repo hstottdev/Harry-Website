02550b729449499a9274835811d80dcc78a062c3
Harry Stott <harrystott.dev@gmail.com>
2024-09-09 11:22:32 +0100

    removed device check for now

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.onload = function() {
     function update() {
         updateTime();
         getDiscordData();   
-        deviceCheck();
+        //deviceCheck();
     }
         update();
         setInterval(update, 1000);
@@ -112,7 +112,7 @@ function statusHover(){
 function deviceCheck(){
 
     try {
-        screen.orientation.lock('landscape');
+        //screen.orientation.lock('landscape');
     } catch (error) {
         //nevermind
     }
