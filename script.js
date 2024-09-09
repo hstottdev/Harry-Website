f80c772693a0432ed7981393d3fcf507f4d19135
Harry Stott <harrystott.dev@gmail.com>
2024-09-09 14:26:46 +0100

    removed projects margin for thin screens

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.onload = function() {
     function update() {
         updateTime();
         getDiscordData();   
-        orientationCheck();
+        //orientationCheck();
     }
         update();
         setInterval(update, 1000);
