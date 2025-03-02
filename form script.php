245231d50ceca280c9b16136b511a0192185e174
Harry Stott <115025517+hstottdev@users.noreply.github.com>
2025-03-02 12:38:33 +0000

    changed email to contact email

--- form script.php
+++ form script.php
@@ -2,7 +2,7 @@
 if (isset($_POST['Email'])) {
 
     // EDIT THE FOLLOWING TWO LINES:
-    $email_to = "harrystott.dev@gmail.com";
+    $email_to = "contact@harrystott.dev";
     $email_subject = "New form submissions";
 
     function problem($error)
