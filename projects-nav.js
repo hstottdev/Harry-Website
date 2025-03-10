624b011f6f343a55db7f962514da4ef1f68f3d78
Harry Stott <115025517+hstottdev@users.noreply.github.com>
2025-03-10 16:37:20 +0000

    re-adjusted timeout for carousel scrolling

--- projects-nav.js
+++ projects-nav.js
@@ -73,7 +73,10 @@ function scrollToCarousel(index) {
 
     
     //carouselView.scroll(0, carousels[index].getBoundingClientRect().top);
-    carousels[index].scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' }, 10);
+    setTimeout(() => {
+        carousels[index].scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' })
+    }, 100);
+
     carousels[index].scrollLeft = 0;
     console.log("index: " + index);
     console.log(carousels[index].getBoundingClientRect().top);
