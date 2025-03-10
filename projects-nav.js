abf038fb4704bd2aa9a61853da1138f41941d258
Harry Stott <115025517+hstottdev@users.noreply.github.com>
2025-03-10 16:05:32 +0000

    fixed issue where entire page moves

--- projects-nav.js
+++ projects-nav.js
@@ -73,7 +73,7 @@ function scrollToCarousel(index) {
 
     
     //carouselView.scroll(0, carousels[index].getBoundingClientRect().top);
-    carousels[index].scrollIntoView({ behavior: 'smooth'});
+    carousels[index].scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
     carousels[index].scrollLeft = 0;
     console.log("index: " + index);
     console.log(carousels[index].getBoundingClientRect().top);
